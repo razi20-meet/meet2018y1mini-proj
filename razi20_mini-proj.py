a6a8dd16c69155f67c4b1d2a087468bd5aa4aaec
razi20-meet <razi20@meet.mit.edu>
2018-08-08 03:31:05 -0400

    Stopping here

--- razi20_mini-proj.py
+++ razi20_mini-proj.py
@@ -10,13 +10,13 @@ import turtle
 import random 
 
 turtle.tracer(1,0) 
-
+turtle.pu()
 SIZE_X=800
 SIZE_Y=500
 turtle.setup(SIZE_X, SIZE_Y) 
 
 SQUARE_SIZE = 20
-START_LENGTH = 6
+START_LENGTH = 100
 
 pos_list = []
 stamp_list = []
@@ -24,7 +24,7 @@ food_pos = []
 food_stamps = []
 
 snake = turtle.clone()
-snake.shape("turtle")
+snake.shape("square")
 
 turtle.hideturtle()
 
@@ -67,6 +67,10 @@ RIGHT = 3
 ####WRITE YOUR CODE HERE!!
 
 direction = UP
+UP_EDGE = 250
+DOWN_EDGE = -250
+RIGHT_EDGE = 400
+LEFT_EDGE = -400
 
 def up():
     global direction #snake direction is global (same everywhere)
@@ -101,13 +105,26 @@ def move_snake():
     my_pos = snake.pos()
     x_pos = my_pos[0]
     y_pos = my_pos[1]
-    
+    def move_snake():
+        new_pos = snake.pos()
+        new_x_pos = new_pos[0]
+        new_y_pos = new_pos[1]
+        if new_x_pos >= RIGHT_EDGE:
+            print("You hit the right edge! Game over!")
+            quit()
+
     if direction==RIGHT:
         snake.goto(x_pos + SQUARE_SIZE, y_pos)
         print("You moved right!")
     elif direction==LEFT:
         snake.goto(x_pos - SQUARE_SIZE, y_pos)
         print("You moved left!")
+    elif direction==UP :
+        snake.goto(x_pos,y_pos + SQUARE_SIZE)
+        print ("you moved up")
+    elif direction==DOWN :
+        snake.goto(x_pos,y_pos - SQUARE_SIZE)
+        print ("you moved down")
 
     #4. Write the conditions for UP and DOWN on your own
     ##### YOUR CODE HERE
@@ -125,6 +142,29 @@ def move_snake():
     old_stamp = stamp_list.pop(0)
     snake.clearstamp(old_stamp)
     pos_list.pop(0)
+    
+turtle.register_shape("trash.gif") #Add trash picture
+                      # Make sure you have downloaded this shape 
+                      # from the Google Drive folder and saved it
+                      # in the same folder as this Python script
+
+food = turtle.clone()
+food.shape("trash.gif") 
+
+#Locations of food
+food_pos = [(100,100), (-100,100), (-100,-100), (100,-100)]
+food_stamps = []
+
+# Write code that:
+#1. moves the food turtle to each food position
+#2. stamps the food turtle at that location
+#3. saves the stamp by appending it to the food_stamps list using
+# food_stamps.append(    )
+#4. Don’t forget to hide the food turtle!
+for this_food_pos in food_pos :
+   
+    
+    ####WRITE YOUR CODE HERE!!
 
 
 
